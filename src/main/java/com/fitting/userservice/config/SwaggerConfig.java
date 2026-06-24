9df497fb31959ca63ec3dbd19ff7809da38e3db9
Lukas-ren <lu.duranh@duocuc.cl>
2026-06-23 21:52:23 -0400

    swagger

---
+++ src/main/java/com/fitting/userservice/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.fitting.userservice.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Fitting API")
+                        .version("1.0.0")
+                        .description("Plataforma de e-commerce de ropa")
+                        .contact(new Contact()
+                                .name("Fitting Team")
+                                .email("admin@fitting.com")))
+                .addSecurityItem(new SecurityRequirement()
+                        .addList("Bearer Authentication"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
+}
